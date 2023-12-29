5e90b21098ca38347aa67e8ce4f831ed116c9008
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2023-12-29 01:15:51 -0300

    Mock Nomes

---
+++ MoveInatorForms/Mock/Data.cs
@@ -0,0 +1,28 @@
+﻿namespace MoveInatorForms.Mock
+{
+    public class Data
+    {
+        public static List<string> Nomes => new List<string>
+        {
+            "Agostinho Carrara",
+            "Saitama",
+            "Pricesa Leia",
+            "Indiana Jones",
+            "James Bond",
+            "Optimus Prime",
+            "Nelson Mandela",
+            "Oprah Winfrey",
+            "Papa Francisco",
+            "Bill Gates",
+            "Jim Halpert",
+            "Dwight Schrute",
+            "Michael Scott",
+            "Itachi Uchiha",
+            "Monkey D. Luffy",
+            "Ayrton Senna",
+            "Ivete Sangalo",
+            "Rodrigo Hilbert",
+            "Loro José"
+        };
+    }
+}
