1232fa4dbe8700020b2a6f0c0d7633480306f7dd
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2023-12-29 00:55:11 -0300

    Modelzinho

---
+++ MoveInatorForms/Models/ViagemViewModel.cs
@@ -0,0 +1,34 @@
+﻿namespace MoveInatorForms.Models
+{
+    public class ViagemViewModel
+    {
+        public int TipoViagem { get; set; }
+
+        public int NumeroViagem { get; set; }
+
+        public int SerieViagem { get; set; }
+
+
+        public int TipoDocumento { get; set; }
+
+        public int NumeroDocumento { get; set; }
+
+        public int SerieDocumento { get; set; }
+
+        public int Quantidade { get; set; }
+
+
+        public DateTime DataEmissao { get; set; }
+
+        public string CnpjEmissor { get; set; }
+
+
+        public string NomeDestinatario { get; set; }
+        
+        public string CpfCnpjDestinatario { get; set; }
+        
+        public string NomeMotorista { get; set; }
+        
+        public string CpfMotorista { get; set; }
+    }
+}
