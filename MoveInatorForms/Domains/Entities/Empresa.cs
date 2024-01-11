4a26f84d9a7bf57b2a44f2596a3152afbc385e03
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-11 12:56:48 -0300

    #6 feat: Criação das entidades

---
+++ MoveInatorForms/Domains/Entities/Empresa.cs
@@ -0,0 +1,16 @@
+﻿namespace MoveInatorForms.Domains.Entities
+{
+    public class Empresa
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cnpj { get; set; }
+
+
+        public string ClientId { get; set; }
+
+        public string ClientSecrect { get; set; }
+    }
+}
---
+++ MoveInatorForms/Domains/Entities/Motorista.cs
@@ -0,0 +1,13 @@
+﻿namespace MoveInatorForms.Domains.Entities
+{
+    public class Motorista
+    {
+        public Guid Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Cpf { get; set; }
+
+        public string Password { get; set; }
+    }
+}
