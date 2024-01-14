28d60c9c41228ea2e919366e362cb420fb58d96a
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-14 12:23:43 -0300

    Correção no nome do Artefato gerado pelo Deploy

--- Scripts/Deploy.ps1
+++ Scripts/Deploy.ps1
@@ -15,7 +15,7 @@ $pathTemplates = [Path]::Combine($pathProject, 'MoveInatorForms', 'Templates');
 Copy-Item -Path "$pathTemplates\*" -Destination $pathArtefatos;
 
 $version = (Get-Item '..\Artefatos\MoveInatorForms.exe').VersionInfo.FileVersion;
-$filePath = "$pathProject\MoveInator_$version.zip";
+$filePath = "$pathProject\MoveInator_Pre-Release@$version.zip";
 
 Start-Process 'C:\Program Files\7-Zip\7z.exe' -ArgumentList @('a', '-tzip', $filePath, "$pathArtefatos\*");
 
