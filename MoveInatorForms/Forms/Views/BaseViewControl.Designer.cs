fca188d0247eb7649eddcb4c30ad3d55f64c6e71
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-25 05:34:29 -0300

    Adicionado BaseViewControl

---
+++ MoveInatorForms/Forms/Views/BaseViewControl.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace MoveInatorForms.Forms.Views
+{
+    partial class BaseViewControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // BaseViewControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(31, 31, 31);
+            Font = new Font("MesloLGL NF", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            ForeColor = Color.DarkGray;
+            Margin = new Padding(0);
+            Name = "BaseViewControl";
+            Size = new Size(600, 600);
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
