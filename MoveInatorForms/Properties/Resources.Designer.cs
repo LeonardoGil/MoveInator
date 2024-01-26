3343696309c9a20f4bf76ed7b945bafdde9096f7
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-01-26 04:42:55 -0300

    Reformulação da tela de Tokens

--- MoveInatorForms/Properties/Resources.Designer.cs
+++ MoveInatorForms/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace MoveInatorForms.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap requisicao_16 {
+            get {
+                object obj = ResourceManager.GetObject("requisicao-16", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap vassoura_16 {
             get {
                 object obj = ResourceManager.GetObject("vassoura-16", resourceCulture);
