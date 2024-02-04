aa4a6a28b2e685e0c06739a4f26a737976545a9b
Leonardo Albuquerque Gil <leonardo.gil@ndd.tech>
2024-02-04 14:19:16 -0300

    Input TextBox

---
+++ Libraries/UtilityLib/Extensions/FloatExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System.Drawing;
+
+namespace UtilityLib.Extensions
+{
+    public static class FloatExtensions
+    {
+        public static float Around(this float value, float weight) => Convert.ToSingle(Math.Round(value / weight) * weight);
+    }
+}
---
+++ MoveInatorForms/Forms/Controls/Input.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using UtilityLib.Extensions;
+
+namespace MoveInatorForms.Forms.Controls
+{
+    public class Input : TextBox
+    {
+        private float weight = 0.25F;
+
+        private readonly Font defaultFont;
+
+        public override Color BackColor { get => Parent?.BackColor ?? Color.White; set => base.BackColor = value; }
+
+        public override Color ForeColor { get => Parent?.ForeColor ?? Color.Black; set => base.ForeColor = value; }
+
+        [Category(nameof(Input))]
+        public float Weight 
+        { 
+            get => weight; 
+            set => weight = value; 
+        }
+
+        public Input()
+        {
+            defaultFont = Font;
+            BorderStyle = BorderStyle.FixedSingle;
+        }
+
+        public void ResizeFont(float percentage)
+        {
+            var size = (defaultFont.Size + (defaultFont.Size * percentage)).Around(weight);
+            Font = new Font(defaultFont.FontFamily, size, defaultFont.Style, defaultFont.Unit);
+        }
+    }
+}
