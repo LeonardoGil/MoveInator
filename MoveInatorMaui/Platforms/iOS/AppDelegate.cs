53019bd887cd34b50130f43388d1d5b9c4e56570
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-03-31 14:13:26 -0300

    Adicionado projeto MoveInatorMaui (Default Template)

---
+++ MoveInatorMaui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MoveInatorMaui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
