b5aa9f9d1315d4566b8739bbcf2e897dcd65cddb
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-04-11 06:20:51 -0300

    Gerando menu Flyout

--- MoveInatorMaui/MauiProgram.cs
+++ MoveInatorMaui/MauiProgram.cs
@@ -15,9 +15,9 @@ namespace MoveInatorMaui
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+//#if DEBUG
+//    		builder.Logging.AddDebug();
+//#endif
 
             return builder.Build();
         }
---
+++ MoveInatorMaui/Views/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MoveInatorMaui.Views;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
