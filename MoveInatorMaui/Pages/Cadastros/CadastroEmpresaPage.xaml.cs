1b075aed6ef6aacac9791fa59702207f97272d13
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-05-10 20:47:17 -0300

    Desenvolvimento

--- MoveInatorMaui/Pages/Cadastros/CadastroEmpresaPage.xaml.cs
+++ MoveInatorMaui/Pages/Cadastros/CadastroEmpresaPage.xaml.cs
@@ -1,19 +1,35 @@
+using MoveInatorDomain.Entities.Cadastros;
 using MoveInatorDomain.Models;
 
 namespace MoveInatorMaui.Pages.Cadastros;
 
 public partial class CadastroEmpresaPage : ContentPage
 {
-	public CadastroEmpresaViewModel ViewModel { get; set; } = new CadastroEmpresaViewModel();
+    public CadastroEmpresaViewModel ViewModel { get; set; } = new CadastroEmpresaViewModel();
+
+    public List<CadastroEmpresaViewModel> Empresas { get; set; } = new List<CadastroEmpresaViewModel>();
 
     public CadastroEmpresaPage()
-	{
-		InitializeComponent();
-		BindingContext = ViewModel;
-	}
+    {
+        InitializeComponent();
+        BindingContext = new
+        {
+            ViewModel,
+            Empresas
+        };
+    }
 
     private async void Salvar_Clicked(object sender, EventArgs e)
     {
-		await DisplayAlert("Teste", ViewModel.RazaoSocial ?? "Nothing", "Ok né");
+        //var empresa = new Empresa
+        //{
+        //    RazaoSocial = ViewModel.RazaoSocial,
+        //    Cnpj = ViewModel.Cnpj,
+        //    ClientId = ViewModel.ClientId,
+        //    ClientSecrect = ViewModel.ClientSecrect
+        //};
+
+        Empresas.Add(ViewModel);
+        ViewModel = new CadastroEmpresaViewModel();
     }
 }
