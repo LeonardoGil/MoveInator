491e8b5834f249a721ef5c3d01b90f00189c4477
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-06-03 23:06:05 -0300

    Initialization of fields in importacaoXmlPage

--- MoveInatorMaui/Pages/Importacoes/ImportacaoXmlPage.xaml.cs
+++ MoveInatorMaui/Pages/Importacoes/ImportacaoXmlPage.xaml.cs
@@ -1,9 +1,36 @@
+using CommunityToolkit.Maui.Core.Extensions;
+using MoveInatorApplication.Services.Interfaces;
+using MoveInatorMaui.ViewModels;
+
 namespace MoveInatorMaui.Pages.Importacoes;
 
 public partial class ImportacaoXmlPage : ContentPage
 {
-    public ImportacaoXmlPage()
+    private readonly IMotoristaService _motoristaService;
+    private readonly IEmpresaService _empresaService;
+
+    protected ImportacaoXmlViewModel ViewModel { get; set; } = new ImportacaoXmlViewModel();
+
+    public ImportacaoXmlPage(IMotoristaService motoristaService, 
+                             IEmpresaService empresaService)
     {
+        _motoristaService = motoristaService;
+        _empresaService = empresaService;
+
+        BindingContext = ViewModel;
+
         InitializeComponent();
+        InitializePickers();
+    }
+
+    private void InitializePickers()
+    {
+        ViewModel.ListaMotorista = _motoristaService.GetModels().ToObservableCollection();
+        ViewModel.ListaEmpresa = _empresaService.GetModels().ToObservableCollection();
+    }
+
+    private void GenerateFile_Clicked(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
     }
 }
---
+++ MoveInatorMaui/ViewModels/ImportacaoXmlViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MoveInatorApplication.Models;
+using System.Collections.ObjectModel;
+
+namespace MoveInatorMaui.ViewModels
+{
+    public partial class ImportacaoXmlViewModel : ObservableValidator
+    {
+        [ObservableProperty]
+        public ObservableCollection<EmpresaModel> listaEmpresa = new();
+
+        [ObservableProperty]
+        public ObservableCollection<MotoristaModel> listaMotorista = new();
+    }
+}
